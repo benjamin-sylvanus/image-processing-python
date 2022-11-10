09499df108ce587fe5767012b54cd56511c27870
Benjamin Sylvanus <Sylvanus.b@northeastern.edu>
2022-11-09 19:55:13 -0500

    face and eye tracking with opencv

--- tracking.py
+++ tracking.py
@@ -6,6 +6,10 @@ while (1):
     _, frame = cap.read()
     # Convert BGR to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    ret, thresh = cv.threshold(
+        grey, 200, 255, cv.THRESH_BINARY_INV+cv.THRESH_TRIANGLE)
+
     # define range of blue color in HSV
     lower_blue = np.array([100, 60, 50])
     upper_blue = np.array([150, 255, 255])
@@ -13,9 +17,13 @@ while (1):
     mask = cv.inRange(hsv, lower_blue, upper_blue)
     # Bitwise-AND mask and original image
     res = cv.bitwise_and(frame, frame, mask=mask)
+    m = cv.bitwise_and(frame, frame, mask=thresh)
+    can = cv.Canny(hsv, 100, 150)
     cv.imshow('frame', frame)
     cv.imshow('mask', mask)
     cv.imshow('res', res)
+    cv.imshow('m', m)
+
     k = cv.waitKey(100)
     if k == 27:
         break
